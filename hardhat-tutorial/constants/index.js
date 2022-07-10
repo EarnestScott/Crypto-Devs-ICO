301c3e75b80757b7163aea1eec62bcfa34bc11d5
Earnest Scott <earnestdscott@gmail.com>
2022-07-10 12:08:02 -0700

    Fully functional ICO proj from tutorial

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xBF97c694e03f8322178FC3E4291A3E00dAb912e4";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
